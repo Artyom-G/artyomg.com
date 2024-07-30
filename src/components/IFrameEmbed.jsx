37078d00cee3dceb47bd126d590446f1949b631e
Artyom Gabtraupov <artyomgv05@gmail.com>
2024-07-30 19:27:20 -0400

    iframe embed overlay

--- src/components/IFrameEmbed.jsx
+++ src/components/IFrameEmbed.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useState } from 'react';
 import './IFrameEmbed.scss';
 
 const IFrameEmbed = ({ src, width, height, scrolling='no' }) => {
+    const [overlayActive, setOverlayActive] = useState(true);
+    
     return (
         <div className="iframeembed">
+            <div className={`${overlayActive ? 'iframeembed__overlay' : 'iframeembed__overlay__disabled'}`} onClick={() => setOverlayActive(false)} style={{width: width, height: height}}>
+                Click Me
+            </div>
             <iframe className="iframeembed__iframe" src={src} width={width} height={height} frameborder='0' scrolling={scrolling}></iframe>
             <div className="iframeembed__bigger-display-message">
                 <p>This Page Has an Iteractive Element, Please Use a Bigger Screen</p>
